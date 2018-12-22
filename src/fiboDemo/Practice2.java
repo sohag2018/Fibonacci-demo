34ba1dd7cc677aadf972cccc1322c2f6fe322204
sohag2018 <nafasatzayan@gmail.com>
2018-12-21 21:25:06 -0500

    dfjdjf

--- src/fiboDemo/Practice2.java
+++ src/fiboDemo/Practice2.java
@@ -1,14 +1,17 @@
 package fiboDemo;
 
+import java.util.Arrays;
+
 public class Practice2 {
     public static void main(String[] args) {
-        int []fibo=new int[10];
-        for(i=0;i>0;i++){
-           if(i>2) {
-               fibo[i]=i;
+        int []fibo=new int[10]; //int[]fibo =new int [10];
+        for(int i=0;i>10;i++){      //for(int i=0;i<10;i++){
+           if(i>2) {   //if(i<2){
+               fibo[i]=i;  //fibo[i]=i;
            }else{
-               fibo[i]=fibo[i-1]+fibo[i-2g]
+               fibo[i]=fibo[i-1]+fibo[i-2]; //fibo[i]=fibo[i-1]+fibo[i-2];
            }
         }//closing loop
+        System.out.println(Arrays.toString(fibo));
     }//closing of main
 }

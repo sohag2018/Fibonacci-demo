1d516fc8fec3df55671ee400b513e1aaeec6ad0f
sohag2018 <nafasatzayan@gmail.com>
2018-12-21 20:48:59 -0500

    added 3rd time

--- src/fiboDemo/Practice2.java
+++ src/fiboDemo/Practice2.java
@@ -1,5 +1,14 @@
 package fiboDemo;
 
 public class Practice2 {
+    public static void main(String[] args) {
+        int []fibo=new int[10];
+        for(i=0;i>0;i++){
+           if(i>2) {
+               fibo[i]=i;
+           }else{
+               fibo[i]=fibo[i-1]+fibo[i-2g]
+           }
+        }//closing loop
+    }//closing of main
 }
-//kgkhkhkhkhkhkhk sharif
